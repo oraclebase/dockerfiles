da9bedbb4e62b951783d9448bdf49ce46411dd3b
Tim Hall <tim@oracle-base.com>
2020-05-12 22:31:59 +0000

    Switch to microdnf and add shadow-utils.

--- ords/ol8_ords/scripts/install_os_packages.sh
+++ ords/ol8_ords/scripts/install_os_packages.sh
@@ -2,7 +2,7 @@ echo "**************************************************************************
 echo "Install OS Packages." `date`
 echo "******************************************************************************"
 # fontconfig : Added to support OpenJDK inside container.
-dnf install -y unzip tar gzip freetype fontconfig ncurses
-dnf update -y
+microdnf install -y unzip tar gzip freetype fontconfig ncurses shadow-utils
+microdnf update -y
 rm -Rf /var/cache/yum
 rm -Rf /var/cache/dnf
