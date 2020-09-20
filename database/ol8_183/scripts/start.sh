3877dd28a188dc1eb0aa0cb39bf1af0c0baf1e4a
Kevin Nguyen <concuadong89@gmail.com>
2020-09-21 00:24:56 +0700

    Update start.sh
    adding the missing datafile for apex tablespace

--- database/ol8_183/scripts/start.sh
+++ database/ol8_183/scripts/start.sh
@@ -152,7 +152,7 @@ EOF
 
     sqlplus / as sysdba <<EOF
 alter session set container = ${PDB_NAME};
-create tablespace apex datafile size 1m autoextend on next 1m;
+create tablespace apex datafile "/u02/oradata/apex01.dbf" size 1m autoextend on next 1m;
 @apexins.sql APEX APEX TEMP /i/
 
 BEGIN
