3f79bcfea827f0d32634d0068a9d9f213e725b76
Tim Hall <tim@oracle-base.com>
2021-08-18 08:54:00 +0100

    Switch to install oracle-database-preinstall-21c now it has been released.

--- database/ol8_21/scripts/install_os_packages.sh
+++ database/ol8_21/scripts/install_os_packages.sh
@@ -2,7 +2,7 @@ echo "**************************************************************************
 echo "Install OS Packages." `date`
 echo "******************************************************************************"
 microdnf install -y unzip tar gzip shadow-utils
-microdnf install -y oracle-database-preinstall-19c
+microdnf install -y oracle-database-preinstall-21c
 microdnf update -y
 rm -Rf /var/cache/yum
 rm -Rf /var/cache/dnf
