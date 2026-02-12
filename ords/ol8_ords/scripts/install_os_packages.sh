11bf486a7fd55fd397db2a1a875bb80c14869623
Tim Hall <t.hall.1@bham.ac.uk>
2026-02-12 12:15:55 +0000

    Add findutils and ncurses.

--- ords/ol8_ords/scripts/install_os_packages.sh
+++ ords/ol8_ords/scripts/install_os_packages.sh
@@ -2,7 +2,7 @@ echo "**************************************************************************
 echo "Install OS Packages." `date`
 echo "******************************************************************************"
 # fontconfig : Added to support OpenJDK inside container.
-microdnf install -y unzip tar gzip freetype fontconfig ncurses shadow-utils
+microdnf install -y unzip tar gzip freetype fontconfig ncurses shadow-utils findutils
 microdnf update -y
 rm -Rf /var/cache/yum
 rm -Rf /var/cache/dnf
