a4f61fe78c8321ba9eda7945e8c22f9e7efb1f26
Tim Hall <t.hall.1@bham.ac.uk>
2026-02-12 12:15:30 +0000

    Add findutils and ncurses.

--- ords/ol7_ords/scripts/install_os_packages.sh
+++ ords/ol7_ords/scripts/install_os_packages.sh
@@ -2,6 +2,6 @@ echo "**************************************************************************
 echo "Install OS Packages." `date`
 echo "******************************************************************************"
 # fontconfig : Added to support OpenJDK inside container.
-yum install -y unzip tar gzip freetype fontconfig
+yum install -y unzip tar gzip freetype fontconfig findutils ncurses
 yum update -y
 rm -Rf /var/cache/yum
